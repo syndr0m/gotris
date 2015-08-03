87cb38d2fbe5e02f68eb39349b23a3e13f1b0116
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-08-04 01:44:18 +0200

    refactoring intro, using Assets() & Boards() api

--- intro.go
+++ intro.go
@@ -7,7 +7,7 @@ import (
 	"mygameengine/image"
 )
 
-func drawBackground(screen *image.Image, frame uint64, imageIntroRotatingBg *image.Image) {
+func drawIntroBackground(screen *image.Image, frame uint64, imageIntroRotatingBg *image.Image) {
 	var y int = int(math.Mod(float64(frame), 217))
 
 	screen.BlitAt(imageIntroRotatingBg, 0, y-217)
@@ -18,7 +18,7 @@ func drawBackground(screen *image.Image, frame uint64, imageIntroRotatingBg *ima
 
 const maskDuration = 2 // seconds
 
-func drawMask(screen *image.Image, frame uint64, fps uint) {
+func drawIntroMask(screen *image.Image, frame uint64, fps uint) {
 	var maxFrame uint64 = uint64(maskDuration * fps)
 
 	if frame < maxFrame {
@@ -28,17 +28,35 @@ func drawMask(screen *image.Image, frame uint64, fps uint) {
 	}
 }
 
-func NewBoardIntro(engine *mygameengine.MyGameEngine) *mygameengine.Board {
-	imageIntroRotatingBg, _ := image.Png("assets/images/intro-rotating-bg.png") // 640x217
-	imageIntroTitle, _ := image.Png("assets/images/intro-title.png")            // 380x120
+func drawIntroText(screen *image.Image, frame uint64, imageIntroText *image.Image) {
+	var v int = int(math.Mod(float64(frame), 50))
+	if v > 25 {
+		screen.BlitAt(imageIntroText, 210, 370)
+	}
+}
 
-	intro := engine.NewBoard()
-	intro.OnKeyDown(func(key int) { fmt.Println("main: KEY DOWN") })
+func Intro(engine *mygameengine.MyGameEngine) *mygameengine.Board {
+	engine.Assets().Png(IMG_ROTATING_BG)
+	engine.Assets().Png(IMG_TITLE)
+	engine.Assets().Png(IMG_PRESS_SPACE)
+
+	intro := mygameengine.NewBoard()
+	intro.OnKeyDown(func(key int) {
+		fmt.Println("intro: KEY DOWN", key)
+		if key == 1 {
+			engine.Boards().SetCurrent(engine.Boards().Get("game"))
+		}
+	})
 	intro.OnRepaint(func(screen *image.Image) {
 		frame := intro.GetFrame()
-		drawBackground(screen, frame, imageIntroRotatingBg)
-		screen.BlitAt(imageIntroTitle, 130, 140)
-		drawMask(screen, frame, engine.GetFps())
+		drawIntroBackground(screen, frame, engine.Assets().Get(IMG_ROTATING_BG))
+		screen.BlitAt(engine.Assets().Get(IMG_TITLE), 130, 140)
+		drawIntroText(screen, frame, engine.Assets().Get(IMG_PRESS_SPACE))
+		drawIntroMask(screen, frame, engine.GetFps())
 	})
 	return intro
 }
+
+const IMG_ROTATING_BG string = "assets/images/intro-rotating-bg.png" // 640x217
+const IMG_TITLE string = "assets/images/intro-title.png"             // 380x120
+const IMG_PRESS_SPACE string = "assets/images/intro-press-space-to-start.png"
